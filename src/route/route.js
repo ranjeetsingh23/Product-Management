403f9a8574737c5103277e61e4d4b20289678112
Ranjeetsingh23 <Ranjeetsinghdua@gmail.com>
2022-08-01 16:45:46 +0530

    Route

--- src/route/route.js
+++ src/route/route.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const userController = require("../controller/userController");
 const productController = require("../controller/productController");
 const cartController = require('../controller/cartController')
-const mid = require("../middleware/auth")
+const mid = require("../middleware/auth");
 
 // ---------------------------------------- USER API's -------------------------------------------
 router.post("/register", userController.createUser)
